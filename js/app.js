3238866c1dd0f775c16485b7308926576a3cad47
karly92 <karla.p.s.c10@gmail.com>
2014-12-18 15:22:34 -0600

    primera version con tabs sin controller

---
+++ js/app.js
@@ -0,0 +1,47 @@
+(function(){
+//Modulo de Angular.js que se ejecutará dentro de mi codigo html
+	var app = angular.module('store',[]);
+	app.controller('StoreController',function(){
+		this.products = gems;
+
+	});
+
+	var gems =[ 
+	{
+		name: 'Dodecahedron',
+		price: 2,
+		description: '. . .',
+		images: [
+			{
+				full: 'img/cupcake1.jpg',
+				thumb: 'img/cupcake1_thumbnail.jpg'
+			},
+			{
+				full: 'img/cupcake2.jpg',
+				thumb: 'img/cupcake2_thumbnail.jpg'
+			},
+		],
+		canPurchase: true,
+		soldOut: false,
+	},
+	{
+		name: 'Pentagonal Gem',
+		price: 5.95,
+		description: '. . .',
+		images: [
+			{
+				full: 'img/cupcake2.jpg',
+				thumb: 'img/cupcake2_thumbnail.jpg'
+			},
+			{
+				full: 'img/cupcake1.jpg',
+				thumb: 'img/cupcake1_thumbnail.jpg'
+			},
+		],
+		canPurchase: false,
+		soldOut: false,
+	}
+];
+
+
+})();
